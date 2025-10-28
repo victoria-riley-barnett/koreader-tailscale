356da40afcea0746fef27d2a7e0a729ca88bb7d2
Victoria <9009959+victoria-riley-barnett@users.noreply.github.com>
2025-10-27 21:05:37 -0700

    name

--- _meta.lua
+++ _meta.lua
@@ -3,7 +3,7 @@ return {
     name = "tailscale",
     fullname = _("Tailscale VPN"),
     description = _("Secure remote access and file sync via Tailscale"),
-    author = "Kindle Tailscale Plugin",
+    author = "Victoria B.",
     version = "1.0",
     dependencies = {},
     can_configure = true,
